83e4e71016d172e937064a6d03673b0364eee1b6
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2024-01-10 21:19:34 +0100

    improve plotting efficiency

--- task3/explore.py
+++ task3/explore.py
@@ -5,6 +5,7 @@ from glob import glob
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 from pypdf import PdfMerger
 from tqdm import tqdm
 
@@ -30,28 +31,29 @@ def _plot_frames(args):
     label = data["label"] if "label" in data else None
     dataset = f"({data['dataset']})" if "dataset" in data else ""
 
-    if movement is not None:
-        movement = np.where(movement > 0.01, 2.0, np.nan)
-
-    if label is not None:
-        label = np.where(label, 3.0, np.nan)
-
     vmin, vmax = video.min(), video.max()
     mpl.rcParams.update(mpl.rcParamsDefault)
     for j in range(len(video)):
         fig, ax = plt.subplots(num=1, clear=True)
-        ax.imshow(video[j], cmap="gray", vmin=vmin, vmax=vmax)
+        ax.imshow(video[j], cmap="gray", vmin=vmin, vmax=vmax, interpolation=None)
         if box is not None:
             ax.contour(box, levels=[0.5], colors="tab:blue")
         if movement is not None:
-            ax.imshow(movement[j], cmap="tab10", vmin=0.5, vmax=10.5, alpha=0.6)
+            ax.imshow(movement[j], cmap=alpha_cmap("Oranges"), alpha=0.6, interpolation=None)
         if label is not None:
-            ax.imshow(label[j], cmap="tab10", vmin=0.5, vmax=10.5, alpha=0.6)
+            ax.imshow(label[j], cmap=alpha_cmap("Greens"), alpha=0.6, interpolation=None)
         ax.set_axis_off()
         ax.set_title(f"{dataset} frame: {j + 1:4d}/{len(video)}")
         fig.savefig(f"{prefix}_{i:04d}_{j:04d}.pdf", bbox_inches="tight")
 
     merge_pdfs(f"{prefix}_{i:04d}")
+
+
+def alpha_cmap(cmap):
+    colors = plt.get_cmap(cmap)([0, 255])
+    colors[:, -1] = np.linspace(0.0, 1.0, 2)
+    cmap = LinearSegmentedColormap.from_list("alpha_" + cmap, colors)
+    return cmap
 
 
 def merge_pdfs(prefix):
