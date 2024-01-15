08935d21327f69ad8cb0eaa3fc3951c0ab3331cf
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2024-01-15 21:39:54 +0100

    adjust _plot_frames; plot 2x4 during training

--- task3/explore.py
+++ task3/explore.py
@@ -39,9 +39,9 @@ def _plot_frames(args):
         if box is not None:
             ax.contour(box, levels=[0.5], colors="tab:blue")
         if movement is not None:
-            ax.imshow(movement[j], cmap=alpha_cmap("Oranges"), vmin=0.0, vmax=1.0)
+            ax.imshow(movement[j], cmap=alpha_cmap("Oranges"), vmin=0.0, vmax=1.0, alpha=0.8)
         if label is not None:
-            ax.imshow(label[j], cmap=alpha_cmap("Greens"), vmin=0.0, vmax=1.0, alpha=0.5)
+            ax.contour(label[j], levels=[0.5], colors="tab:green")
         ax.set_axis_off()
         ax.set_title(f"{dataset} frame: {j + 1:4d}/{len(video)}")
         fig.savefig(f"{prefix}_{i:04d}_{j:04d}.pdf", bbox_inches="tight")
@@ -60,9 +60,11 @@ def plot_intermediate(loader, model, epoch, n_epochs, loss, prefix):
     device = next(model.parameters()).device
     x, y = next(iter(loader))
     pred = sigmoid(model(x.to(device)))
-    fig, axs = plt.subplots(4, 4, num=1, clear=True, figsize=(10, 10))
+    fig, axs = plt.subplots(2, 4, num=1, clear=True, figsize=(10, 5))
+    x, y, pred = map(lambda x: x.squeeze().cpu().numpy(), (x, y, pred))
+    if x.ndim == 4:
+        x = x[:, 0, :, :]
     for ax, xi, yi, pi in zip(axs.flatten(), x, y, pred):
-        xi, yi, pi = map(lambda x: x.squeeze().cpu().numpy(), (xi, yi, pi))
         ax.imshow(xi, cmap="gray")
         ax.contour(yi, levels=[0.5], colors="tab:blue")
         ax.contour(pi, levels=[0.5], colors="tab:orange")
